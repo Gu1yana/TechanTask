3d049560783d77f06b52b99f29eeb56571404e0c
Gu1yana <gulyanamk-bp110@code.edu.az>
2025-05-02 07:36:35 +0400

    techanMigration

---
+++ Techan/DataAccessLayer/TechanDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Techan.Models;
+
+namespace Techan.DataAccessLayer;
+
+public class TechanDbContext:DbContext
+{
+    public DbSet<Slider> Sliders { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=WIN-G1CGFOVK2JI\\SQLEXPRESS;Database=Techan;Trusted_Connection=true;TrustServerCertificate=true");
+        base.OnConfiguring(optionsBuilder);     
+    }
+
+}
--- Techan/obj/Debug/net8.0/Techan.AssemblyInfo.cs
+++ Techan/obj/Debug/net8.0/Techan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Techan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa402c6dded6dc1da24cf4e8be4b8805e8128234")]
 [assembly: System.Reflection.AssemblyProductAttribute("Techan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Techan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
